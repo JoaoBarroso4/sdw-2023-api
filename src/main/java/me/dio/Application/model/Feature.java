7cfbe10a046a33bb4df6f96318383f324be31a1e
Joao <joao.barroso5@aluno.ce.gov.br>
2023-07-25 13:45:55 -0300

    Implementação da camada de domínio

---
+++ src/main/java/me/dio/Application/model/Feature.java
@@ -0,0 +1,8 @@
+package me.dio.Application.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/Application/model/News.java
@@ -0,0 +1,7 @@
+package me.dio.Application.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}
